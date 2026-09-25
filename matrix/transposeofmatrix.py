69d836ca204a47124517fff3f7b2b75df5a1907f
Pradeep sanwaraya <pradeepsanwaraya1670@gmail.com>
2026-09-25 20:40:51 +0530

    done

--- matrix/transposeofmatrix.py
+++ matrix/transposeofmatrix.py
@@ -1,11 +1,29 @@
-# n=[[1,2,3],[4,5,6]]
-# r=len(n)
-# c=len(n[0])
-# result = [[0]*r for i in range(c)]
-# for i in range(r):
-#     for j in range(c):
-#         result[j][i]=n[i][j]
-# print(result,end="")
+# # n=[[1,2,3],[4,5,6]]
+# # r=len(n)
+# # c=len(n[0])
+# # result = [[0]*r for i in range(c)]
+# # for i in range(r):
+# #     for j in range(c):
+# #         result[j][i]=n[i][j]
+# # print(result,end="")
+# # n=int(input("enter a size"))
+# # res=[]
+# # for i in range(n):
+# #     temp=[]
+# #     for j in range(n):
+# #         x=int(input("enter a list"))
+# #         temp.append(x)
+# #     res.append(temp)
+# # print(res)
+# # r=len(res)
+# # c=len(res[0])
+# # for i in range(r):
+# #     for j in range(c):
+# #         if j>=i:
+# #             print(res[i][j],end=" ")
+# #         else:
+# #             print("*",end=" ")
+# #     print()
 # n=int(input("enter a size"))
 # res=[]
 # for i in range(n):
@@ -14,39 +32,52 @@
 #         x=int(input("enter a list"))
 #         temp.append(x)
 #     res.append(temp)
+# n2=int(input("enter a size"))
+# res2=[]
+# for i in range(n2):
+#     temp2=[]
+#     for j in range(n2):
+#         x2=int(input("enter a list"))
+#         temp2.append(x2)
+#     res2.append(temp2)
 # print(res)
 # r=len(res)
 # c=len(res[0])
+# result=[]
 # for i in range(r):
+#     temp=[]
 #     for j in range(c):
-#         if j>=i:
-#             print(res[i][j],end=" ")
-#         else:
-#             print("*",end=" ")
-#     print()
-n=int(input("enter a size"))
-res=[]
-for i in range(n):
-    temp=[]
-    for j in range(n):
-        x=int(input("enter a list"))
-        temp.append(x)
-    res.append(temp)
-n2=int(input("enter a size"))
-res2=[]
-for i in range(n2):
-    temp2=[]
-    for j in range(n2):
-        x2=int(input("enter a list"))
-        temp2.append(x2)
-    res2.append(temp2)
-print(res)
-r=len(res)
-c=len(res[0])
-result=[]
-for i in range(r):
-    temp=[]
-    for j in range(c):
-        temp.append(res[i][j]+res2[i][j])
-    result.append(temp)
-print(result)
+#         temp.append(res[i][j]+res2[i][j])
+#     result.append(temp)
+# print(result)
+
+class Employee:
+    def __init__(self,id,name,salary):
+        self.id=id
+        self.name=name
+        self.salary=salary
+    def increase_salary(self):
+        self.salary=self.salary+500
+        print(self.salary)
+    def display(self):
+        print(self.id)
+        print(self.name)
+        print(self.salary)
+class Developer(Employee):
+    def __init__(self,id,name,salary,language):
+        super().__init__(id,name,salary)
+        self.language=language
+    def add(self):
+        print("developer is codeing",self.name)
+class Manager(Employee):
+
+    def __init__(self,id,name,salary,team):
+        super().__init__(id,name,salary)
+        self.team=team
+
+    def manage(self):
+        print("manager is managing",self.team,"members")
+obj=Manager(1,"pp",5000,10)
+obj.display()
+obj.increase_salary()
+obj.manage()
